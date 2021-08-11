faa70cc4e275fd3f771633df14f955b5e16439c5
PaulJD88 <77843480+PaulJD88@users.noreply.github.com>
2021-08-11 21:01:52 +0100

    First Experiment

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
+    <h1> Nest </h1>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld subtitle="Monkey" msg="Welcome Paul Dixon"/>
+    <h1> Nest </h1>
+    <HelloWorld msg="Welcome Tom"/>
+    <ShoppingList />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ShoppingList from './components/ShoppingList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ShoppingList
   }
 }
 </script>
---
+++ src/components/ShoppingList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    <h1>Shopping List</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShoppingList',
+}
+</script>
+
+<style>
+</style>
